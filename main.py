9384a2b449fd861a507ba72984a9563ff4602147
Federico Cardozo <fedecar94@outlook.com>
2021-06-16 21:47:18 -0400

    minimun change

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-def selection_sort(array: list) -> list:
+def selection_sort(array: list):
     size = len(array)
-    if not size:
-        return []
 
     for index in range(size):
         min_value = array[index]
         min_index = index
 
-        for jndex in range(index, size):
+        for jndex in range(index + 1, size):
             if array[jndex] < min_value:
                 min_value = array[jndex]
                 min_index = jndex
@@ -15,13 +13,12 @@ def selection_sort(array: list) -> list:
         if min_index != index:
             array[index], array[min_index] = array[min_index], array[index]
 
-    return array
-
 
 if __name__ == '__main__':
     import random
-    randomlist = []
-    for i in range(0, 10):
-        n = random.randint(1, 30)
-        randomlist.append(n)
-    print(selection_sort(randomlist))
+
+    randomlist = [random.randint(1, 30) for x in range(0, 10)]
+
+    print(randomlist)
+    selection_sort(randomlist)
+    print(randomlist)
